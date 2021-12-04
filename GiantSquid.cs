e7e31c0858ae23f0a43a6068c05cbea228e3290d
selmaohneh <marcel@neumann.sh>
2021-12-04 12:58:35 +0100

    Fix me up. First lines of code.

---
+++ GiantSquid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    internal class GiantSquid
+    {
+        public void Part1(string input)
+        {
+            var splitInput = input.Split(Environmennt.NewLine);
+            var randomNumbers = splitInput[0];
+
+        }
+    }
+
+    [TestClass]
+    public class Day4
+    {
+        [TestMethod]
+        public void Day4_Part1()
+        {
+            var sut = new GiantSquid();
+
+            int result = sut.Part1(Resources.Day4);
+
+            Assert.AreEqual(1636725, result);
+        }
+    }
+}
